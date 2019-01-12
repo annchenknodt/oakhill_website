38fb47555b282e29d2238f7f141da1f84c4def41
annchenruth <aknodt@gmail.com>
2019-01-12 14:45:19 -0500

    moved Debug to settings_secret

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #SECRET_KEY = '' # in settings_secret.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True # in settings_secret
 
 ALLOWED_HOSTS = ['www.oakhillhomesolutions.com','127.0.0.1','69.163.224.179','oakhillhomesolutions.com']
 
