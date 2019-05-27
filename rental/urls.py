eb6cc1daf807dab2a73a392d329ef4619d351652
annchenruth <aknodt@gmail.com>
2019-05-27 12:52:55 -0400

    added sliders and now troubleshooting NaN/infinity

--- rental/urls.py
+++ rental/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import login, logout
+from django.views.generic import TemplateView
+
 
 from . import views
 
@@ -13,5 +15,6 @@ urlpatterns = [
     # path('<int:question_id>/results/', views.results, name='results'),
     path('<int:analysis_id>/results/', views.results, name='results'),
   	path('<int:analysis_id>/edit/', views.edit, name='edit'),
+  	path('test_slider', TemplateView.as_view(template_name='test_slider.html'),name="test_slider"),
     
 ]
